c780ec8f52861c083664daf2a3eb98058e17b12e
xiaojing <995283168@qq.com>
2018-10-10 22:31:44 +0800

    modify

--- orangutang/src/main/java/com/orangutang/orangutang/config/Swagger2Conf.java
+++ orangutang/src/main/java/com/orangutang/orangutang/config/Swagger2Conf.java
@@ -1,4 +1,37 @@
 package com.orangutang.orangutang.config;
 
-public class t {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * swagger conf
+ */
+@Configuration
+public class Swagger2Conf {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.orangutang.orangutang.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("orangutang")
+                .description("orangutang api")
+                .termsOfServiceUrl("design by orangutang")
+                .version("1.0")
+                .build();
+    }
 }
